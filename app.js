bab77caa3c09fbdfb7a8eacf7113d01119ea11af
Julia Glinka <julia.w.glinka@gmail.com>
2024-02-23 18:01:13 +0100

    Import fs module and add four tables as a data in app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require ('fs');
+
+const genders = [ 'M' , 'F' ];
+const maleNames = [ 'John', 'Arthur', 'Tom', 'Frank', 'Carl' ];
+const femaleNames = [ 'Julia', 'Samantha', 'Anastasia', 'Alexandra', 'Barbara' ];
+const lastNames = [ 'Smith', 'Roberts', 'Edison', 'Anderson', 'Whiliams']
