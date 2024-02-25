c579c01719e8e9112fdca3dc76f0f864ccf7e2dc
Julia Glinka <julia.w.glinka@gmail.com>
2024-02-25 13:53:15 +0100

    Add func randChoice

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const genders = [ 'M' , 'F' ];
 const maleNames = [ 'John', 'Arthur', 'Tom', 'Frank', 'Carl' ];
 const femaleNames = [ 'Julia', 'Samantha', 'Anastasia', 'Alexandra', 'Barbara' ];
 const lastNames = [ 'Smith', 'Roberts', 'Edison', 'Anderson', 'Whiliams']
+
+const randChoice = (arr) => {
+    if (arr.length === 0) {
+        return undefined;
+    }
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+};
